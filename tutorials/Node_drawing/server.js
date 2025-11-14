3fa6b84a616aea9ddc33702e9e2ab4d4d83ad6d1
crabino <76144314+crabino@users.noreply.github.com>
2025-11-13 16:42:25 -0800

    wgew

--- tutorials/Node_drawing/server.js
+++ tutorials/Node_drawing/server.js
@@ -1,1 +1,24 @@
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+var socket = require('socket.io');
+
 console.log("hello world");
+var io =  socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+    
+    console.log('new connection' + socket.id);
+    socket.on('mouse', mouseMsg);
+}
+
+function mouseMsg(data){
+    socket.broadcast.emit('mouse', data);
+    console.log(data);
+}
